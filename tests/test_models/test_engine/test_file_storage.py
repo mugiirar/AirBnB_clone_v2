fe3eacdc668789b7dddfb8e80eb3c393cfd5e01c
mugiirar <munenevincentmugiira@gmail.com>
2023-12-19 10:13:11 -0800

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,16 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import os
+import pep8
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
 class test_fileStorage(unittest.TestCase):
