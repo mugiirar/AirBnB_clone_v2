e1d477cfcaaeeb0e0f4863287c872fa095c5ab2e
mugiirar <munenevincentmugiira@gmail.com>
2024-01-10 21:52:07 -0800

    test

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Prepare web servers
+
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Hello you" > /data/web_static/releases/test/index.html
+
+link_path="/data/web_static/current"
+target_path="/data/web_static/releases/test"
+
+if [ -L "$link_path" ]; then
+    rm "$link_path"
+fi
+
+ln -s "$target_path" "$link_path"
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
